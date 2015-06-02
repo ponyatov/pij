7b0305db13e376c4fc3c28aa33d8ca30ca24480b
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-02 17:54:47 +0400

    ? loop not works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pij2d.pas
+++ pij2d.pas
@@ -67,16 +67,19 @@ Var
 ///////////  If Z0*1000>90 then Z0:=Z0-0.090;
 ///////////  Until Z0<0.09;
  
- Y:=Trunc(Z0*1000);
- X:=10-Trunc(R*1000);//X:=11;
- //Ez:=0;
- Ez:=(Fi[x,y]-Fi[x,y+1])*8000/0.001;
- Er:=-1*(Fi[x,y]-Fi[x+1,y])*8000/0.001;
- Z:=Z+Vz*dt+Ez*qm*dt*dt/2;
- Vz:=Vz+Ez*qm*dt;
- R:=R+Vr*dt+Er*qm*dt*dt/2;
- Vr:=Vr+Er*qm*dt;
- t:=t+dt;
+///////////  Y:=Trunc(Z0*1000);
+///////////  X:=10-Trunc(R*1000);//X:=11;
+///////////   //Ez:=0;
+
+/////////// Ez:=(Fi[x,y]-Fi[x,y+1])*8000/0.001;
+/////////// Er:=-1*(Fi[x,y]-Fi[x+1,y])*8000/0.001;
+/////////// Z:=Z+Vz*dt+Ez*qm*dt*dt/2;
+
+///////////  Vz:=Vz+Ez*qm*dt;
+/////////// R:=R+Vr*dt+Er*qm*dt*dt/2;
+/////////// Vr:=Vr+Er*qm*dt;
+/////////// t:=t+dt;
+ 
  append(te);
  write(te,z:8:6);
  write(te,' ');
