42d7c9a7a55da62e8789f9494b57cded77fea8ce
Dmitry Ponyatov <dponyatov@gmail.com>
2015-07-03 15:23:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- resPov_visualize.py
+++ resPov_visualize.py
@@ -5,18 +5,23 @@ class vtkPointCloud:
     def __init__(self):
         cone = vtkConeSource()
         cone.Update()
+        self.points = vtkPoints()
         self.actor = vtkActor()
         self.polydata = vtkPolyData()
+        self.polydata.SetPoints(self.points)
         self.mapper = vtkPolyDataMapper()
-        self.mapper.SetInputData(cone.GetOutput())
+        self.mapper.SetInputData(self.polydata)
         self.actor.SetMapper(self.mapper)
+    def add(self,x,y,z,value):
+        pass
 
 cloud = vtkPointCloud()
 
 F = open('resPot1mm.txt', 'r')
 F.readlines(2)  # skip head
 for line in F.readlines(4):
-    X, Y, Z, P = map(float, line.split())
+    X,Y,Z,P=map(float, line.split())
+    cloud.add(X,Y,Z,P)
 F.close()
 
 # Visualize
